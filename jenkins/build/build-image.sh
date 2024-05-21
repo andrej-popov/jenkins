9454b41c6ccfe7135b2c1aaa61e014dcea098080
Andrej Popov <58514770+Andrejpopof@users.noreply.github.com>
2024-05-21 17:09:38 +0200

    test

--- jenkins/build/build-image.sh
+++ jenkins/build/build-image.sh
@@ -3,5 +3,6 @@
 #COPY NEW JAR TO THE BUILD DIRECTORY
 cp -f $PWD/simple-java-maven-app/target/*jar jenkins/build/
 
+docker build -t maven-project:13 jenkins/build/Dockerfile
 #BUILD THE IMAGE USING THE GENERATED JAR FILE
-docker-compose -f jenkins/build/docker-compose-build.yaml build
+#docker-compose -f jenkins/build/docker-compose-build.yaml build
