a577d9469252c21ea17ca5a387f384c061cf5fad
Andrej Popov <58514770+Andrejpopof@users.noreply.github.com>
2024-05-21 16:49:24 +0200

    nesto

--- jenkins/build/mvn-build.sh
+++ jenkins/build/mvn-build.sh
@@ -4,8 +4,8 @@ echo "************************"
 echo "**** BUILDING A JAR ****"
 echo "************************"
 
-#WORKSPACE=/var/jenkins_home/workspace/pipeline-maven-docker
+WORKSPACE=/var/jenkins_home/workspace/pipeline-maven-docker
 #echo $WORKSPACE
 #echo "pwd: $(pwd)"
 # SCRIPT FOR BUILDING THE JAR FILE AND TESTING THE JAVA APPLICATION [mvn -B -DskipTests clean package ; mvn test]
-docker run -it -v $PWD/simple-java-maven-app:/app -v /root/.m2:/root/.m2 -w /app maven sh #mvn -B -DskipTests clean package
+docker run -it -w /app -v $PWD/simple-java-maven-app:/app -v /root/.m2:/root/.m2 maven sh #mvn -B -DskipTests clean package
