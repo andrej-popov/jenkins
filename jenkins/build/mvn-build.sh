83529fbe5e85574aedac19f79b1078da8bc26c33
Andrej Popov <58514770+Andrejpopof@users.noreply.github.com>
2024-05-22 13:54:08 +0200

    fix1

--- jenkins/build/mvn-build.sh
+++ jenkins/build/mvn-build.sh
@@ -4,13 +4,13 @@ echo "************************"
 echo "**** BUILDING A JAR ****"
 echo "************************"
 
-WORKSPACE=/mnt/workspace/pipeline-mvn
+WORKSPACE=/mnt/workspace/pipeline-mvn-1
 #echo $WORKSPACE
 #echo "pwd: $(pwd)"
 
 
-ls -l $WORKSPACE/simple-java-mvn
-docker run --rm -w /app -v $WORKSPACE/simple-java-mvn-app:/app -v /root/.m2:/root/.m2 arm64v8/maven ls -l /app
+ls -l $WORKSPACE/simple-java-mvn-1
+#docker run --rm -w /app -v $WORKSPACE/simple-java-mvn-app:/app -v /root/.m2:/root/.m2 arm64v8/maven ls -l /app
 
 # SCRIPT FOR BUILDING THE JAR FILE AND TESTING THE JAVA APPLICATION [mvn -B -DskipTests clean package ; mvn test]
 docker run --rm -w /app -v $WORKSPACE/simple-java-maven-app:/app -v /root/.m2:/root/.m2 arm64v8/maven "$@"
