77124b0741a934d53916fa02c0b6bd398dc52735
Andrej Popov <58514770+Andrejpopof@users.noreply.github.com>
2024-05-22 15:28:22 +0200

    fix6

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -3,7 +3,7 @@
 
 #SCP THE FILE CONTAINING THE VARIABLES TO THE REMOTE SERVER
 echo maven-project > /tmp/.auth
-echo $BUILD_TAG >> /tmp/.auth
+echo "16" >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
 echo "******************"
@@ -13,8 +13,8 @@ echo "******************"
 cat /tmp/.auth
 
 #IF I HAVE THIS SCRIPT ON JENKINS SERVER DO BELOW
-scp -i "logiranje.pem" .auth ec2-user@ec2-3-68-29-229.eu-central-1.compute.amazonaws.com:/tmp/.auth 'ls -l /tmp/.auth'
-scp -i "logiranje.pem" publish.sh ec2-user@ec2-3-68-29-229.eu-central-1.compute.amazonaws.com:/tmp/
+scp -i /tmp/logiranje.pem .auth ec2-user@ec2-3-68-29-229.eu-central-1.compute.amazonaws.com:/tmp/.auth 'ls -l /tmp/.auth'
+scp -i /tmp/logiranje.pem publish.sh ec2-user@ec2-3-68-29-229.eu-central-1.compute.amazonaws.com:/tmp/
 
-ssh -i "logiranje.pem" ec2-user@ec2-3-68-29-229.eu-central-1.compute.amazonaws.com '/tmp/publish.sh'
+ssh -i /tmp/logiranje.pem ec2-user@ec2-3-68-29-229.eu-central-1.compute.amazonaws.com '/tmp/publish.sh'
 
