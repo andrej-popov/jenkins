075101b5eeb997a01b305cd148987b41b7079a2c
Andrej Popov <58514770+Andrejpopof@users.noreply.github.com>
2024-05-22 13:37:12 +0200

    fix

--- jenkins/build/mvn-build.sh
+++ jenkins/build/mvn-build.sh
@@ -10,7 +10,7 @@ WORKSPACE=/mnt/workspace/pipeline-mvn-app
 
 
 ls -l $WORKSPACE/simple-java-maven-app
-docker run --rm -w /app -v $WORKSPACE/simple-java-maven-app:/app:z -v /root/.m2:/root/.m2 arm64v8/maven ls -l /app
+docker run --rm -w /app -v $WORKSPACE/simple-java-mvn-app:/app -v /root/.m2:/root/.m2 arm64v8/maven ls -l /app
 
 # SCRIPT FOR BUILDING THE JAR FILE AND TESTING THE JAVA APPLICATION [mvn -B -DskipTests clean package ; mvn test]
 docker run --rm -w /app -v $WORKSPACE/simple-java-maven-app:/app -v /root/.m2:/root/.m2 arm64v8/maven "$@"
