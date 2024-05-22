fddf015d42f0cefb5ccd8216ff48b3829c944be5
Andrej Popov <58514770+Andrejpopof@users.noreply.github.com>
2024-05-22 18:11:06 +0200

    test4

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -14,7 +14,7 @@ cat /tmp/.auth
 
 #IF I HAVE THIS SCRIPT ON JENKINS SERVER DO BELOW
 scp -i /tmp/logiranje.pem /tmp.auth ec2-user@ec2-3-68-29-229.eu-central-1.compute.amazonaws.com:/tmp/.auth 'ls -l /tmp/.auth'
-scp -i /tmp/logiranje.pem publish.sh ec2-user@ec2-3-68-29-229.eu-central-1.compute.amazonaws.com:/tmp/
+scp -i /tmp/logiranje.pem /tmp/publish.sh ec2-user@ec2-3-68-29-229.eu-central-1.compute.amazonaws.com:/tmp/
 
 ssh -i /tmp/logiranje.pem ec2-user@ec2-3-68-29-229.eu-central-1.compute.amazonaws.com '/tmp/publish.sh'
 
--- jenkins/deploy/publish.sh
+++ jenkins/deploy/publish.sh
@@ -4,6 +4,10 @@ export IMAGE=$(sed -n '1p' /tmp/.auth)
 export TAG=$(sed -n '2p' /tmp/.auth)
 export PASS=$(sed -n '3p' /tmp/.auth)
 
+echo $IMAGE
+echo $TAG
+echo $PASS
+
 docker login -u andrejpopov -p $PASS
 
 cd /tmp && docker-compose up -d
