a1cc6a122ee14caa0137067373c3ed86c2fa3dbc
Andrej Popov <58514770+Andrejpopof@users.noreply.github.com>
2024-05-22 17:58:05 +0200

    changed deploy script

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -3,7 +3,7 @@
 
 #SCP THE FILE CONTAINING THE VARIABLES TO THE REMOTE SERVER
 echo maven-project > /tmp/.auth
-echo "16" >> /tmp/.auth
+echo $TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
 echo "******************"
@@ -13,7 +13,7 @@ echo "******************"
 cat /tmp/.auth
 
 #IF I HAVE THIS SCRIPT ON JENKINS SERVER DO BELOW
-scp -i /tmp/logiranje.pem .auth ec2-user@ec2-3-68-29-229.eu-central-1.compute.amazonaws.com:/tmp/.auth 'ls -l /tmp/.auth'
+scp -i /tmp/logiranje.pem /tmp.auth ec2-user@ec2-3-68-29-229.eu-central-1.compute.amazonaws.com:/tmp/.auth 'ls -l /tmp/.auth'
 scp -i /tmp/logiranje.pem publish.sh ec2-user@ec2-3-68-29-229.eu-central-1.compute.amazonaws.com:/tmp/
 
 ssh -i /tmp/logiranje.pem ec2-user@ec2-3-68-29-229.eu-central-1.compute.amazonaws.com '/tmp/publish.sh'
