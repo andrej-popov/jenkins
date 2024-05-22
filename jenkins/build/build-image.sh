c9e04e6e912215dfb057bc7767e489a56b6ed320
Andrej Popov <58514770+Andrejpopof@users.noreply.github.com>
2024-05-22 16:04:05 +0200

    test1

--- jenkins/build/build-image.sh
+++ jenkins/build/build-image.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-
-
 #COPY NEW JAR TO THE BUILD DIRECTORY
-cp -f $PWD/simple-java-maven-app/target/*jar jenkins/build/
+cp -f ./simple-java-maven-app/target/*jar jenkins/build/
 
-docker build -t maven-project:15 $PWD/jenkins/build/
+docker build -t maven-project:15 ./jenkins/build/
 
 #BUILD THE IMAGE USING THE GENERATED JAR FILE
 #docker-compose -f jenkins/build/docker-compose-build.yaml build
--- jenkins/build/mvn-build.sh
+++ jenkins/build/mvn-build.sh
@@ -4,13 +4,11 @@ echo "************************"
 echo "**** BUILDING A JAR ****"
 echo "************************"
 
-#WORKSPACE=/mnt/workspace/pipeline-mvn-1
+WORKSPACE=mnt/jenkins/workspace/pipeline-mvn-1
 #echo $WORKSPACE
 #echo "pwd: $(pwd)"
 
-
-ls -l $PWD/simple-java-mvn-1
 #docker run --rm -w /app -v $WORKSPACE/simple-java-mvn-app:/app -v /root/.m2:/root/.m2 arm64v8/maven ls -l /app
 
 # SCRIPT FOR BUILDING THE JAR FILE AND TESTING THE JAVA APPLICATION [mvn -B -DskipTests clean package ; mvn test]
-docker run --rm -w /app -v $PWD/simple-java-maven-app:/app -v /root/.m2:/root/.m2 arm64v8/maven "$@"
+docker run --rm -w /app -v $WORKSPACE/simple-java-maven-app:/app -v /root/.m2:/root/.m2 arm64v8/maven "$@"
