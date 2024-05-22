77af19df61a85286ea94eaa97d7242938dc10be1
Andrej Popov <58514770+Andrejpopof@users.noreply.github.com>
2024-05-22 12:00:20 +0200

    Change workspace

--- jenkins/build/mvn-build.sh
+++ jenkins/build/mvn-build.sh
@@ -13,4 +13,4 @@ ls -l $WORKSPACE/simple-java-maven-app
 docker run --rm -w /app -v $WORKSPACE/simple-java-maven-app:/app:z -v /root/.m2:/root/.m2 arm64v8/maven ls -l /app
 
 # SCRIPT FOR BUILDING THE JAR FILE AND TESTING THE JAVA APPLICATION [mvn -B -DskipTests clean package ; mvn test]
-docker run --rm -w /app -v $WORKSPACE/simple-java-maven-app:/app -v /root/.m2:/root/.m2 arm64v8/maven mvn -B -DskipTests clean package
+docker run --rm -w /app -v $WORKSPACE/simple-java-maven-app:/app -v /root/.m2:/root/.m2 arm64v8/maven "$@"
